74d4fbc6e0b34e3d9ebdd103728711110df313c7
zlj <1978501178@qq.com>
2022-03-26 15:52:07 +0800

    third commit idea

--- src/main/java/com/zlj/test.java
+++ src/main/java/com/zlj/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("你好 git");
         System.out.println("你好 git2");
+        System.out.println("你好 git3");
     }
 }

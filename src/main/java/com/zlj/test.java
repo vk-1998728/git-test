db87134314468291b1197242ed2992f6725842c8
vk-1998728 <101616896+vk-1998728@users.noreply.github.com>
2022-04-12 06:15:06 +0800

    Update test.java
    这是为了制造冲突

--- src/main/java/com/zlj/test.java
+++ src/main/java/com/zlj/test.java
@@ -2,14 +2,6 @@ package com.zlj;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("你好 git");
-        System.out.println("你好 git2");
-        System.out.println("你好 git3");
-        System.out.println("你好 git4");
-
-        System.out.println("你好 git5");
-        System.out.println("push test");
-        System.out.println("httpspush test5");
-        System.out.println("pull test5");
+       System.out.println("玩你妈的git");
     }
 }

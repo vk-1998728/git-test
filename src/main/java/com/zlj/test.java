7280b3c7669bf09787d1e6e413731061f1aa83b2
zlj <1978501178@qq.com>
2022-04-18 06:42:09 +0800

    先commit再pushaacc

--- src/main/java/com/zlj/test.java
+++ src/main/java/com/zlj/test.java
@@ -3,6 +3,6 @@ package com.zlj;
 public class test {
     public static void main(String[] args) {
        System.out.println("玩你妈的git");
-       System.out.println("4-18更新代码aa");
+       System.out.println("4-18更新代码aacc");
     }
 }
